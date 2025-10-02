d410faa444ce3d0e8a7ee2b0fd50ff980d2e6ce2
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-02 19:26:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ updateDarkModeBtn();
 // Project filtering
 const projects = [
   { title: 'Portfolio Website', category: 'Web', desc: 'A modern responsive portfolio.', img: 'images/portfolio.png', links: [{ url: 'https://ashensilva.netlify.app/', label: 'View' }] },
+   { title: 'Smart Menu and Inventory Management System', category: 'Web', desc: '.', img: 'images/home.png', links: [{ url: 'https://ashensilva.netlify.app/', label: 'View' }] },
 ];
 const categories = ['All', ...new Set(projects.map(p => p.category))];
 const filterButtons = document.getElementById('filterButtons');
